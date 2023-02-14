163beaf03fcb50fe98bc3fff77286426e1f9deba
Eduard Smet <96624179+Celarye@users.noreply.github.com>
2023-02-14 11:48:51 +0100

    Added a script to receive the data over Wi-Fi.

---
+++ Website/src/data.py
@@ -0,0 +1,29 @@
+import socket
+
+# Define the IP address and port to listen on
+IP_ADDRESS = '0.0.0.0'
+PORT = 5555
+
+# Create a socket object
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+
+# Bind the socket to the IP address and port
+s.bind((IP_ADDRESS, PORT))
+
+# Listen for incoming connections
+s.listen()
+
+# Accept a connection and receive data indefinitely
+while True:
+    # Accept a connection from a client
+    client_socket, address = s.accept()
+    print(f'Connection from {address[0]}:{address[1]} has been established.')
+
+    # Receive data from the client
+    data = client_socket.recv(1024)
+
+    # Print the received data
+    print(f'Received data: {data.decode()}')
+
+    # Close the connection
+    client_socket.close()
