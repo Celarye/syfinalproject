f8bc00bea12fe7efb2afe8251e59a497de9bb97f
Eduard Smet <96624179+Celarye@users.noreply.github.com>
2023-02-14 21:31:55 +0100

    Added a script that shows the data locally.

---
+++ Raspberry_Pi_4_B/src/local.py
@@ -0,0 +1,45 @@
+"""WPSE342 sensor script"""
+import time
+import smbus2
+
+# Initialize I2C bus
+bus = smbus2.SMBus(1)
+
+# WPSE342 sensor address
+ADDRESS = 0xb5
+
+def read_temperature():
+    """Read temperature."""
+    data = bus.read_i2c_block_data(ADDRESS, 0xE3, 2)
+    temp = ((data[0] << 8) + data[1]) / 100
+    return temp
+
+def read_humidity():
+    """Read humidity."""
+    data = bus.read_i2c_block_data(ADDRESS, 0xE5, 2)
+    humidity = ((data[0] << 8) + data[1]) / 1024
+    return humidity
+
+def read_co2():
+    """Read CO2."""
+    data = bus.read_i2c_block_data(ADDRESS, 0xE8, 2)
+    co2 = ((data[0] << 8) + data[1]) / 1024
+    return co2
+
+def read_tvoc():
+    """Read TVOC."""
+    data = bus.read_i2c_block_data(ADDRESS, 0xE7, 2)
+    tvoc = ((data[0] << 8) + data[1]) / 1024
+    return tvoc
+
+# Main loop
+while True:
+    finaltemp = read_temperature()
+    finalhumidity = read_humidity()
+    finalCO2 = read_co2()()
+    finalTVOC = read_tvoc()
+
+    # Construct a message with the data
+    print(f'Temperature: {finaltemp} C, Humidity: {finalhumidity}%, CO2: {finalCO2} ppm, TVOC: {finalTVOC} ppb')
+
+    time.sleep(300)
