ec922749241f17e57fcbe760df2b2518e3b60c5e
Eduard Smet <96624179+Celarye@users.noreply.github.com>
2023-04-25 12:00:51 +0200

    Website wip

--- website/src/App.js
+++ website/src/App.js
@@ -1,23 +1,34 @@
-import logo from './includes/logo.svg';
+import logo from './includes/plant-icon.png';
+import data from './data/data.json';
 import './App.css';
+
+console.log(data);
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <div>
+          <img src={logo} className="App-logo" alt="logo" />
+          <h>Plants Management</h>
+        </div>
       </header>
+      <body>
+        <div>
+          <h>Global Values:</h>
+          <p>{data.global.temp.at(-1)}</p>
+          <h>Plant specific values:</h>
+          <p>\Data</p>
+        </div>
+      </body>
+      <footer>
+        <div>
+          <p>
+            Made by Celarye using{' '}
+            <a href="https://create-react-app.dev/">Create React App</a>
+          </p>
+        </div>
+      </footer>
     </div>
   );
 }
