1f51db440aa8e93f89114886dd6f1dd551d67705
Eduard Smet <96624179+Celarye@users.noreply.github.com>
2023-05-24 22:23:19 +0200

    Updated the script so it works for 3 SM sensors

--- sensors/src/sensors.py
+++ sensors/src/sensors.py
@@ -6,16 +6,12 @@ import json
 import csv
 import logging
 import board
-from flask import Flask, send_file
 import adafruit_ads1x15.ads1015 as ADS
 from adafruit_ads1x15.analog_in import AnalogIn
 from adafruit_bme280 import basic as adafruit_bme280
 
 logging.basicConfig(level=logging.INFO, format='%(levelname)s: %(message)s')
 logger = logging.getLogger(__name__)
-
-NUM_TRACKED_PLANTS = None
-CALIBRATION_FILE = 'config.json'
 
 i2c = board.I2C()
 
@@ -24,153 +20,120 @@ ads = ADS.ADS1015(i2c)
 bme280 = adafruit_bme280.Adafruit_BME280_I2C(i2c)
 logger.info("BME280 connected.")
 
-if not os.path.isfile(CALIBRATION_FILE):
-    num_tracked_plants = input("Enter the number of tracked plants (1-3): ")
-    NUM_TRACKED_PLANTS = int(num_tracked_plants)
-else:
-    with open(CALIBRATION_FILE, 'r', encoding='UTF-8') as file:
-        calibration_data = json.load(file)
+channels = [AnalogIn(ads, ADS.P0), AnalogIn(
+    ads, ADS.P1), AnalogIn(ads, ADS.P2)]
+sensor_labels = ["Plant 1", "Plant 2", "Plant 3"]
+logger.info("Soil moisture sensors connected.")
 
-    if 'num_tracked_plants' in calibration_data:
-        logger.info("Number of tracked plants: %s",
-                    calibration_data['num_tracked_plants'])
-        update_num_plants = input(
-            "Do you want to update the number of tracked plants? [y/n]: ")
-        if update_num_plants.lower() == 'y':
-            num_tracked_plants = input(
-                "Enter the new number of tracked plants (1-3): ")
-            NUM_TRACKED_PLANTS = int(num_tracked_plants)
-        else:
-            NUM_TRACKED_PLANTS = calibration_data['num_tracked_plants']
-    else:
-        num_tracked_plants = input(
-            "Enter the number of tracked plants (1-3): ")
-        NUM_TRACKED_PLANTS = int(num_tracked_plants)
+DRY_SATURATION = [None, None, None]
+WET_SATURATION = [None, None, None]
 
-channels = [AnalogIn(ads, getattr(ADS, f'P{i}'))
-            for i in range(NUM_TRACKED_PLANTS)]
-logger.info("Soil moisture sensor(s) connected.")
+CALIBRATION_FILE = 'config.json'
 
 try:
     with open(CALIBRATION_FILE, 'r', encoding='UTF-8') as file:
         calibration_data = json.load(file)
 
-    if 'num_tracked_plants' in calibration_data and \
-            calibration_data['num_tracked_plants'] == NUM_TRACKED_PLANTS:
-        for i in range(NUM_TRACKED_PLANTS):
-            wet_saturation = calibration_data['plants'][i]['wet_saturation']
-            dry_saturation = calibration_data['plants'][i]['dry_saturation']
-            setattr(channels[i], 'wet_saturation', wet_saturation)
-            setattr(channels[i], 'dry_saturation', dry_saturation)
-    else:
-        logger.info(
-            'Calibration data mismatch. Starting calibration process...')
+    wet_saturation_data = calibration_data.get('wet_saturation')
+    dry_saturation_data = calibration_data.get('dry_saturation')
+
+    if wet_saturation_data and dry_saturation_data:
+        for i in range(len(channels)):
+            WET_SATURATION[i] = wet_saturation_data[i]
+            DRY_SATURATION[i] = dry_saturation_data[i]
+
+        logger.info('Calibration data loaded from the config file.')
+        logger.info(calibration_data)
 
 except FileNotFoundError:
     logger.info('Calibration file not found. Starting calibration process...')
-
-    for i in range(NUM_TRACKED_PLANTS):
-        logger.info("Calibrating soil moisture sensor for Plant %d...", i + 1)
-
-        dry_check = input(f"Is Capacitive Sensor Dry for Plant {i + 1}? [y]: ")
+    for i, channel in enumerate(channels):
+        dry_check = input(
+            f"Is Capacitive Sensor for {sensor_labels[i]} Dry? [y]: ")
         if dry_check.lower() == 'y':
-            setattr(channels[i], 'dry_saturation', channels[i].value)
+            DRY_SATURATION[i] = channel.value
             logger.info("%5s\t%5s", 'raw', 'v')
-            logger.info("%5d\t%5.3f", channels[i].value, channels[i].voltage)
-
+            logger.info("%5d\t%5.3f", channel.value, channel.voltage)
         for x in range(0, 9):
             time.sleep(30)
-            if channels[i].value > getattr(channels[i], 'dry_saturation'):
-                setattr(channels[i], 'dry_saturation', channels[i].value)
-            logger.info("%5d\t%5.3f", channels[i].value, channels[i].voltage)
+            if channel.value > DRY_SATURATION[i]:
+                DRY_SATURATION[i] = channel.value
+            logger.info("%5d\t%5.3f", channel.value, channel.voltage)
 
         wet_check = input(
-            f"Is Capacitive Sensor in Water for Plant {i + 1}? [y]: ")
+            f"Is Capacitive Sensor for {sensor_labels[i]} in Water? [y]: ")
         if wet_check.lower() == 'y':
-            setattr(channels[i], 'wet_saturation', channels[i].value)
+            WET_SATURATION[i] = channel.value
             logger.info("%5s\t%5s", 'raw', 'v')
-            logger.info("%5d\t%5.3f", channels[i].value, channels[i].voltage)
-
+            logger.info("%5d\t%5.3f", channel.value, channel.voltage)
         for x in range(0, 9):
             time.sleep(30)
-            if channels[i].value < getattr(channels[i], 'wet_saturation'):
-                setattr(channels[i], 'wet_saturation', channels[i].value)
-            logger.info("%5d\t%5.3f", channels[i].value, channels[i].voltage)
+            if channel.value < WET_SATURATION[i]:
+                WET_SATURATION[i] = channel.value
+            logger.info("%5d\t%5.3f", channel.value, channel.voltage)
 
     config_data = {
-        "num_tracked_plants": NUM_TRACKED_PLANTS,
-        "plants": []
+        "sensors": []
     }
 
-    for i in range(NUM_TRACKED_PLANTS):
-        plant_data = {
-            "wet_saturation": getattr(channels[i], 'wet_saturation'),
-            "dry_saturation": getattr(channels[i], 'dry_saturation')
+    for i in range(len(channels)):
+        sensor_config = {
+            "label": sensor_labels[i],
+            "wet_saturation": WET_SATURATION[i],
+            "dry_saturation": DRY_SATURATION[i]
         }
-        config_data["plants"].append(plant_data)
+        config_data["sensors"].append(sensor_config)
 
     logger.info('Saving calibration data to the config file.')
-    with open(CALIBRATION_FILE, 'w', encoding='UTF-8') as outfile:
+    with open('config.json', 'w', encoding='UTF-8') as outfile:
         json.dump(config_data, outfile)
         logger.info(config_data)
 
 logger.info("Starting sensors data logging...")
 
-app = Flask(__name__)
+while True:
+    try:
+        today = datetime.date.today().strftime("%d-%m-%Y")
+        DIRECTORY = "../data/"
+        filename = f"{DIRECTORY}sensorsData_{today}.csv"
 
-today = datetime.date.today().strftime("%d-%m-%Y")
+        os.makedirs(DIRECTORY, exist_ok=True)
 
+        timestamp = time.strftime("%d-%m-%Y %H:%M:%S", time.localtime())
 
-@app.route(f'/data/sensorsData_{today}.csv')
-def serve_csv():
-    """Serves the CSV using Flask"""
-    directory = "../data"
-    filename = f"{directory}/sensorsData_{today}.csv"
+        SAMPLING_INTERVAL = 30
 
-    os.makedirs(directory, exist_ok=True)
+        sensor_readings = []
+        for i, channel in enumerate(channels):
+            soil_moisture_percentage = (
+                channel.value - WET_SATURATION[i]) / (DRY_SATURATION[i]
+                                                      - WET_SATURATION[i]) * 100
+            sensor_readings.append(soil_moisture_percentage)
 
-    timestamp = time.strftime("%d-%m-%Y %H:%M:%S", time.localtime())
+        with open(filename, "a", newline='', encoding='UTF-8') as csvfile:
+            writer = csv.writer(csvfile)
 
-    sampling_interval = 30
+            header = ["Timestamp"] + [f"Soil Moisture ({label})" for label in sensor_labels] + [
+                "Temperature", "Humidity"]
 
-    soil_moisture_readings = []
-    for plant in range(NUM_TRACKED_PLANTS):
-        soil_moisture_label = input(
-            f"Enter the soil moisture label for Plant {plant + 1}: ")
-        soil_moisture_percentage = (
-            channels[plant].value - channels[plant].wet_saturation) / (
-            channels[plant].dry_saturation - channels[plant].wet_saturation) * 100
-        soil_moisture_readings.append(
-            (soil_moisture_label, soil_moisture_percentage))
+            values = [timestamp] + sensor_readings + \
+                [bme280.temperature, bme280.relative_humidity]
 
-    with open(filename, "a", newline='', encoding='UTF-8') as csvfile:
-        writer = csv.writer(csvfile)
+            is_empty = csvfile.tell() == 0
+            if is_empty:
+                writer.writerow(header)
+            writer.writerow(values)
 
-        header = ["Timestamp"]
-        for plant in range(NUM_TRACKED_PLANTS):
-            header.append(
-                f"Soil Moisture ({soil_moisture_readings[plant][0]})")
-        header.extend(["Temperature", "Humidity"])
+        logger.info("Sensors values written to %s at %s.",
+                    filename, timestamp)
+        logger.info("Sensors values: %s", values)
 
-        values = [timestamp]
-        for plant in range(NUM_TRACKED_PLANTS):
-            values.append(soil_moisture_readings[plant][1])
-        values.extend([bme280.temperature, bme280.relative_humidity])
+        time.sleep(SAMPLING_INTERVAL)
 
-        is_empty = csvfile.tell() == 0
-        if is_empty:
-            writer.writerow(header)
-        writer.writerow(values)
+    except IOError:
+        logger.error(
+            "An error occurred while logging the sensors data.", exc_info=True)
 
-    logger.info("Sensors values written to %s at %s.", filename, timestamp)
-    logger.info("Sensors values: %s", values)
-
-    time.sleep(sampling_interval)
-
-    return send_file(filename, as_attachment=True, attachment_filename=f'sensorsData_{today}.csv')
-
-
-if __name__ == '__main__':
-    while True:
-        app.run()
-        time.sleep(60)
+    except KeyboardInterrupt:
+        logger.info('Exiting script.')
+        break
