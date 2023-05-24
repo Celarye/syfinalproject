01c9e230fc050a85ef4b9ea3e2177be5c2147c43
Eduard Smet <96624179+Celarye@users.noreply.github.com>
2023-05-24 23:51:27 +0200

    Website improvements

--- website/src/components/Dashboard.tsx
+++ website/src/components/Dashboard.tsx
@@ -4,7 +4,9 @@ import '../styles/Dashboard.css';
 
 interface SensorData {
   Timestamp: string;
-  'Soil Moisture': string;
+  'Soil Moisture 1': string;
+  'Soil Moisture 2': string;
+  'Soil Moisture 3': string;
   Temperature: string;
   Humidity: string;
 }
@@ -16,11 +18,19 @@ export default function Dashboard() {
     const fetchData = async () => {
       const response = await fetch('http://localhost:5000/');
       const stringValue = await response.text();
-      const [timestamp, soilMoisture, temperature, humidity] =
-        stringValue.split(',');
+      const [
+        timestamp,
+        soilMoisture1,
+        soilMoisture2,
+        soilMoisture3,
+        temperature,
+        humidity,
+      ] = stringValue.split(',');
       setData({
-        Timestamp: timestamp,
-        'Soil Moisture': soilMoisture,
+        Timestamp: timestamp.replace(/[\[\]']+/g, ''),
+        'Soil Moisture 1': soilMoisture1,
+        'Soil Moisture 2': soilMoisture2,
+        'Soil Moisture 3': soilMoisture3,
         Temperature: temperature,
         Humidity: humidity,
       });
@@ -46,18 +56,33 @@ export default function Dashboard() {
               <thead>
                 <tr>
                   <th>Temperature</th>
-                  <th>Soil Moisture</th>
                   <th>Humidity</th>
                 </tr>
               </thead>
               <tbody>
                 <tr>
                   <td>{data.Temperature}</td>
-                  <td>{data['Soil Moisture']}</td>
                   <td>{data.Humidity}</td>
                 </tr>
               </tbody>
             </table>
+            <table>
+              <thead>
+                <tr>
+                  <th>Soil Moisture 1</th>
+                  <th>Soil Moisture 2</th>
+                  <th>Soil Moisture 3</th>
+                </tr>
+              </thead>
+              <tbody>
+                <tr>
+                  <td>{data['Soil Moisture 1']}</td>
+                  <td>{data['Soil Moisture 2']}</td>
+                  <td>{data['Soil Moisture 3']}</td>
+                </tr>
+              </tbody>
+            </table>
+
             <p className="dashboard-fetch-date">
               <i>Last Fetched: {data.Timestamp}</i>
             </p>
